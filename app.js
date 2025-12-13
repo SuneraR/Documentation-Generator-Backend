20bdd9c698409e1f29f4c876015972a72b8116c5
SuneraR <114847925+SuneraR@users.noreply.github.com>
2025-12-13 20:53:44 +0530

    Add final function for passing context to AI model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { askDeepSeekAndGet } from "./utils/ollama.js";
 import { parseRepoUrl } from "./utils/github/parseRepoUrl.js";
+import { extractRepoContext } from "./utils/github/extractRepoContext.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,15 +19,17 @@ app.post("/check", (req, res) => {
 });
 
 app.post("/generate-docs", async (req, res) => {
-  const { code } = req.body;
+  const { link } = req.body;
 
-  const prompt = `
-    You are an expert documentation generator.
-    Read the following source code and generate clear Markdown documentation.
+  const repoContext = await extractRepoContext(link);
 
-    Code:
-    ${code}
-  `;
+  const prompt = `You are an expert documentation generator.
+Analyze the following GitHub repository and generate:
+- README.md
+- Architecture summary
+- API documentation
+${repoContext}
+`;
 
   const output = await askDeepSeekAndGet(prompt);
 
---
+++ utils/github/extractRepoContext.js
@@ -0,0 +1,20 @@
+import { fetchFileContent } from "./fetchFileContent.js";
+import { fetchRepoFiles } from "./fetchRepoFiles.js";
+import { isSourceFile } from "./isSourceFile.js";
+import { parseRepoUrl } from "./parseRepoUrl.js";
+
+export async function extractRepoContext(repoUrl){
+    const{owner,repo}=parseRepoUrl(repoUrl);
+
+    const files=await fetchRepoFiles(owner,repo);
+    const sourceFiles=files.filter(isSourceFile);
+
+    const context=[];
+
+    for(const file of sourceFiles){
+        const content=await fetchFileContent(file);
+
+        context.push(`FILE:${file.path} ${content}`);
+    }
+    return context.json("\n");
+}
